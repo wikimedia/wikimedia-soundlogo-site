02eb6b3658a9c730c19a4b60f29345db080b84fa
goldenapples <goldenapplesdesign@gmail.com>
2022-06-02 15:37:37 -0400

    Add text to edit component for block

--- plugins/wikimedia-contest/blocks/audio-submission-form/index.js
+++ plugins/wikimedia-contest/blocks/audio-submission-form/index.js
@@ -3,6 +3,8 @@
  */
 
 // Start with the same config used on the server side registration.
+import { __ } from '@wordpress/i18n';
+
 import blockConfig from './block.json';
 
 export const name = blockConfig.name;
@@ -10,7 +12,22 @@ export const name = blockConfig.name;
 export const settings = {
 	...blockConfig,
 
-	edit: () => '',
+	/**
+	 * Block edit component.
+	 *
+	 * @returns {React.Component} Block editor render component
+	 */
+	edit: () => (
+		<div className="audio-submission-form">
+			{ __( 'Submission Form', 'wikimedia-contest' ) }
+		</div>
+	),
+
+	/**
+	 * Markup saved to post content.
+	 *
+	 * @returns {React.Component} Markup to save to post content.
+	 */
 	save: () => null,
 
 };
