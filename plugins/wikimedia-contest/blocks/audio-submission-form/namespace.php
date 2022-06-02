f15ce1e5e9bf2390cc46eac9b8a7a20c8916ec75
goldenapples <goldenapplesdesign@gmail.com>
2022-06-02 15:19:54 -0400

    Add filter to allow audio-submission-form block on pages

--- plugins/wikimedia-contest/blocks/audio-submission-form/namespace.php
+++ plugins/wikimedia-contest/blocks/audio-submission-form/namespace.php
@@ -12,6 +12,25 @@ namespace Wikimedia_Contest\Blocks\Audio_Submission_Form;
  */
 function bootstrap() {
 	add_action( 'init', __NAMESPACE__ . '\\register_audio_submission_form', 10, 0 );
+ 	add_filter( 'allowed_block_types', __NAMESPACE__ . '\\filter_blocks_wikimedia_contest', 20, 2 );
+}
+
+/**
+ * Include in the array of allowed blocks the custom blocks
+ * inserted by the Wikimedia Contest plugin.
+ *
+ * @param bool|string[] $allowed_blocks Array of allowed blocks.
+ * @param \WP_Post      $post The post being edited.
+ *
+ * @return string[]
+ */
+function filter_blocks_wikimedia_contest( $allowed_blocks, \WP_Post $post ) : array {
+
+	if ( $post->post_type === 'page' ) {
+		$allowed_blocks[] = 'wikimedia-contest/audio-submission-form';
+	}
+
+	return $allowed_blocks;
 }
 
 /**
