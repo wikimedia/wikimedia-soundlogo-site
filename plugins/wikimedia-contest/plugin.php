7697a29c36748dde93b206d2c2fba3a974c62ec9
Miguel Axcar <miguel@humanmade.com>
2022-06-02 10:11:22 -0300

    Codesniffer suggested changes

--- plugins/wikimedia-contest/plugin.php
+++ plugins/wikimedia-contest/plugin.php
@@ -12,7 +12,7 @@
 
 namespace Wikimedia_Contest;
 
-require_once __DIR__ . '/blocks/audio-submission-form/audio-form.php';
+require_once __DIR__ . '/inc/blocks/audio-submission-form/audio-form.php';
 Custom_Blocks\Audio_Submission_Form\bootstrap();
 
 require_once __DIR__ . '/inc/post-type.php';
