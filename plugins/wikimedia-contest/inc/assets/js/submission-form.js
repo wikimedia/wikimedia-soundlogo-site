19c3e7f7ff3e5493c5cd2e8f0a3876d8e8d00350
Miguel Axcar <miguel@humanmade.com>
2022-06-28 10:56:15 -0300

    Escaping html in frontend

--- plugins/wikimedia-contest/inc/assets/js/submission-form.js
+++ plugins/wikimedia-contest/inc/assets/js/submission-form.js
@@ -7,14 +7,14 @@ jQuery(document).ready(function($){
 		form_data = new FormData();
 
 		// regular fields that need to be processed
-		form_data.append('wiki_username', form.prop('wiki_username').value);
-		form_data.append('legal_name', form.prop('legal_name').value);
-		form_data.append('date_birth', form.prop('date_birth').value);
-		form_data.append('participant_email', form.prop('participant_email').value);
-		form_data.append('phone_number', form.prop('phone_number').value);
-		form_data.append('authors_contributed', form.prop('authors_contributed').value);
-		form_data.append('explanation_creation', form.prop('explanation_creation').value);
-		form_data.append('explanation_inspiration', form.prop('explanation_inspiration').value);
+		form_data.append('wiki_username', document.createTextNode(form.prop('wiki_username').value));
+		form_data.append('legal_name', document.createTextNode(form.prop('legal_name').value));
+		form_data.append('date_birth', document.createTextNode(form.prop('date_birth').value));
+		form_data.append('participant_email', document.createTextNode(form.prop('participant_email').value));
+		form_data.append('phone_number', document.createTextNode(form.prop('phone_number').value));
+		form_data.append('authors_contributed', document.createTextNode(form.prop('authors_contributed').value));
+		form_data.append('explanation_creation', document.createTextNode(form.prop('explanation_creation').value));
+		form_data.append('explanation_inspiration', document.createTextNode(form.prop('explanation_inspiration').value));
 
 		// nonce
 		form_data.append('_submissionnonce', form.prop('_submissionnonce').value);
@@ -35,12 +35,13 @@ jQuery(document).ready(function($){
 			success: function(response_data) {
 				form.hide();
 				if (response_data.status == 'success') {
-					$('#submission_return_message').html(
+					$('#submission_return_message').html(document.createTextNode(
 						response_data.message
 						+ "<br>"
+						+ response_data.submission_date_message
+						+ "<br>"
 						+ response_data.submission_code_message
-						+ response_data.submission_unique_code
-					);
+					));
 				} else {
 					$('#submission_return_message').html(response_data.message);
 				}
