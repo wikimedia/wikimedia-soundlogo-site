a00e3f5c09dadec35c0f4e01c92e07835ed5e4cf
goldenapples <goldenapplesdesign@gmail.com>
2022-07-19 10:00:26 -0400

    Add webpack build to soundlogo theme
    Adds an entry to the production and dev webpack configs to build the
    stylesheet for the soundlogo theme.

--- .webpack/config.prod.js
+++ .webpack/config.prod.js
@@ -1,13 +1,25 @@
 const { helpers, externals, presets } = require( '@humanmade/webpack-helpers' );
 const { filePath } = helpers;
 
-module.exports = presets.production( {
-	externals,
-	name: 'Wikimedia Contest Plugin',
-	entry: {
-		editor: filePath( 'plugins/wikimedia-contest/src/editor.js' ),
-	},
-	output: {
-		path: filePath( 'plugins/wikimedia-contest/build/' ),
-	},
-} );
+module.exports = [
+	presets.production( {
+		externals,
+		name: 'Wikimedia Contest Plugin',
+		entry: {
+			editor: filePath( 'plugins/wikimedia-contest/src/editor.js' ),
+		},
+		output: {
+			path: filePath( 'plugins/wikimedia-contest/build/' ),
+		},
+	} ),
+	presets.production( {
+		externals,
+		name: 'Sound Logo Child Theme',
+		entry: {
+			theme: filePath( 'themes/soundlogo/src/sass/index.scss' ),
+		},
+		output: {
+			path: filePath( 'themes/soundlogo/build/' ),
+		},
+	} ),
+];
