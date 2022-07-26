e09698a290f2b7c4794aa4208324b5585c992dc5
Miguel Axcar <miguel@humanmade.com>
2022-07-26 12:58:26 -0300

    Adding editor styles

--- themes/soundlogo/functions.php
+++ themes/soundlogo/functions.php
@@ -12,12 +12,21 @@ namespace Wikimedia_Contest\Theme;
 use Asset_Loader;
 use Asset_Loader\Manifest;
 
+add_theme_support( 'editor-styles' );
+
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_stylesheets' );
-add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_stylesheets' );
 add_action( 'wp_head', __NAMESPACE__ . '\\embed_fonts' );
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\add_editor_styles' );
 
 require_once __DIR__ . '/inc/editor/namespace.php';
 \Wikimedia_Contest\Theme\Editor\bootstrap();
+
+/**
+ * Add editor styles
+ */
+function add_editor_styles() {
+	add_editor_style( get_stylesheet_directory_uri() . '/src/saas/editor-style.scss' );
+}
 
 /**
  * Enqueue the stylesheet from this theme, as well as the shiro stylesheet.
