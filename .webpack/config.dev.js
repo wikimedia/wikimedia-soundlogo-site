b849f8d43592a50c83c301353c0797f8cdbec299
goldenapples <goldenapplesdesign@gmail.com>
2022-07-27 18:04:25 -0400

    Style basic GF form fields to roughly match mocks

--- .webpack/config.dev.js
+++ .webpack/config.dev.js
@@ -31,6 +31,7 @@ module.exports = choosePort( 8080 ).then( port => [
 		},
 		externals,
 		entry: {
+			themeScripts: filePath( 'themes/soundlogo/src/js/frontend.js' ),
 			frontend: filePath( 'themes/soundlogo/src/sass/frontend.scss' ),
 			editor: filePath( 'themes/soundlogo/src/sass/editor.scss' ),
 		},
