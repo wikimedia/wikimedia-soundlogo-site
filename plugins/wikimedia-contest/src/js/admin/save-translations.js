6b14b64568259f6d3870a632a1432ff382d0d95b
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2022-09-07 14:27:41 -0400

    #643: Allow admins to save translations (#156)
    * Assign RTL styles to labels and GF form elements
    
    Fixes the display of these elements in Arabic and any other RTL
    languages.
    
    * Handle updating translation fields individually
    
    Makes it possible to update translations individually, rather than as
    part of the screening or scoring form submission.
    
    * Properly pass post ID into translation forms

---
+++ plugins/wikimedia-contest/src/js/admin/save-translations.js
@@ -0,0 +1,42 @@
+/**
+ * Interface for saving translations on submissions
+ *
+ * @package
+ */
+
+const translationForms = [ ...document.querySelectorAll( 'form.translation' ) ];
+
+/**
+ * Handle a submission of the translation field form.
+ *
+ * @param {Event} event Submit event.
+ */
+const handleTranslationSubmit = event => {
+	event.preventDefault();
+
+	const { target, submitter } = event;
+
+	submitter.setAttribute( 'disabled', 'disabled' );
+
+	fetch(
+		target.getAttribute( 'action' ),
+		{
+			method: 'POST',
+			body: new URLSearchParams( new FormData( target ) ),
+			credentials: 'same-origin',
+			redirect: 'manual',
+		}
+	).then( response => response.json() )
+		.then( response => {
+			submitter.nextElementSibling.innerHTML = response.data;
+			setTimeout( () => {
+				submitter.removeAttribute( 'disabled' );
+				submitter.nextElementSibling.innerHTML = '';
+			}, 1500 );
+		} );
+};
+
+translationForms.forEach( form => {
+	form.addEventListener( 'submit', handleTranslationSubmit );
+} );
+
